407d57b9b5d0877cbb89c02068394cf666a4afc2
KartoffelChips <104089082+KartoffelChipss@users.noreply.github.com>
2025-11-07 21:12:48 +0100

    Updated login page

--- src/client/components/pages/Login/LoginPage.tsx
+++ src/client/components/pages/Login/LoginPage.tsx
@@ -62,44 +62,55 @@ const LoginPage = () => {
     return (
         <Page title="Login">
             <Layout>
-                <form
-                    className="card items-center p-4 max-w-sm mx-auto flex flex-col gap-4 mt-30"
-                    onSubmit={handleSubmit}
-                >
-                    <img src="/logo.svg" alt="PM2 Dashboard Logo" className="h-14 -mb-2" />
-                    <h2 className="text-4xl gap-1 font-bold">Login</h2>
-                    {error && <FormErrorBox error={error || 'Login failed'} className="w-full" />}
-                    <div className="grid gap-1 w-full">
-                        <label htmlFor="username">Username:</label>
-                        <input
-                            type="text"
-                            className="input"
-                            id="username"
-                            name="username"
-                            required
-                        />
-                    </div>
-                    <div className="grid gap-1 w-full">
-                        <label htmlFor="password">Password:</label>
-                        <input
-                            type="password"
-                            className="input"
-                            id="password"
-                            name="password"
-                            required
-                        />
-                    </div>
-                    <button
-                        type="submit"
-                        className="btn w-full font-semibold"
-                        disabled={loginMutation.isPending}
+                <div className="w-screen h-screen absolute top-0 left-0 flex items-center justify-center -mt-16 p-6">
+                    <form
+                        className="card items-center p-8 py-10 max-w-sm mx-auto flex flex-col gap-6"
+                        onSubmit={handleSubmit}
                     >
-                        Login
-                    </button>
-                    <a className="text-sm text-center text-muted-foreground">
-                        The username and password is set using ENV variables on the server.
-                    </a>
-                </form>
+                        <img src="/logo.svg" alt="PM2 Dashboard Logo" className="h-14 -mb-4" />
+                        <h2 className="text-4xl font-bold">Login</h2>
+                        {error && (
+                            <FormErrorBox error={error || 'Login failed'} className="w-full" />
+                        )}
+                        <div className="grid gap-1 w-full">
+                            {/* <label htmlFor="username">Username:</label> */}
+                            <input
+                                type="text"
+                                className="input"
+                                id="username"
+                                name="username"
+                                placeholder="Username"
+                                required
+                            />
+                        </div>
+                        <div className="grid gap-1 w-full">
+                            {/* <label htmlFor="password">Password:</label> */}
+                            <input
+                                type="password"
+                                className="input"
+                                id="password"
+                                name="password"
+                                placeholder="Password"
+                                required
+                            />
+                        </div>
+                        <button
+                            type="submit"
+                            className="btn w-full font-semibold"
+                            disabled={loginMutation.isPending}
+                        >
+                            Login
+                        </button>
+                        <a
+                            href="https://github.com/KartoffelChipss/pm2-dashboard?tab=readme-ov-file#installation"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="text-sm text-center text-muted-foreground"
+                        >
+                            The username and password is set using ENV variables on the server.
+                        </a>
+                    </form>
+                </div>
             </Layout>
         </Page>
     );
